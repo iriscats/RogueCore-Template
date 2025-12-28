64f4553013f61f7c6561158ac58149dcd4d15c35
iris <2906783821@qq.com>
2025-12-28 22:55:14 +0800

    refactor(MouseCursorWidget): reorganize access modifiers and add missing BlueprintImplementableEvent
    Move bIsHovering to protected section and add missing BlueprintImplementableEvent to OnHover

--- Source/RogueCore/Public/MouseCursorWidget.h
+++ Source/RogueCore/Public/MouseCursorWidget.h
@@ -7,17 +7,24 @@
 UCLASS(Abstract, Blueprintable, EditInlineNew)
 class UMouseCursorWidget : public UFSDUserWidget {
     GENERATED_BODY()
-    
-
 public:
+protected:
     UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     EFSDInputSource TargetInput;
     
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(AllowPrivateAccess=true))
     bool bIsHovering;
+    
+public:
     UMouseCursorWidget();
+
+protected:
     UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
     void OnUnhover();
+    
     UFUNCTION(BlueprintCallable)
     void OnInputSourceChanged(EFSDInputSource Source);
+    
+    UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
     void OnHover();
 };
